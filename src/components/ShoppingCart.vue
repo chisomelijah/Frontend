a305e676cda8fa9fae466a79a60d20a859cd8b4b
Chisom Elijah <stanley.chisom042@gmail.com>
2025-11-26 10:16:14 +0000

    Update fetch URL for orders API in ShoppingCart.vue

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -98,7 +98,7 @@ export default {
           }))
         }
 
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/orders`, {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/orders`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(orderData)
